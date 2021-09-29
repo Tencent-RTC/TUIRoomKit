f2c1518cbfd58db2a7c87ae0e11c56118d7b83b7
tatemin <tatemin@tencent.com>
2021-09-29 15:49:06 +0800

    Android移除androidx迁移说明&增加TUI组件优化参数

--- Android/Source/src/main/java/com/tencent/liteav/meeting/model/impl/trtc/TXTRTCMeeting.java
+++ Android/Source/src/main/java/com/tencent/liteav/meeting/model/impl/trtc/TXTRTCMeeting.java
@@ -15,6 +15,9 @@ import com.tencent.trtc.TRTCCloud;
 import com.tencent.trtc.TRTCCloudDef;
 import com.tencent.trtc.TRTCCloudListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -107,11 +110,25 @@ public class TXTRTCMeeting extends TRTCCloudListener {
         }
     }
 
+    private void setFramework(int framework) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("api", "setFramework");
+            JSONObject params = new JSONObject();
+            params.put("framework", framework);
+            jsonObject.put("params", params);
+            mTRTCCloud.callExperimentalAPI(jsonObject.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void internalEnterRoom() {
         // 进房前设置一下监听，不然可能会被其他信息打断
         if (mTRTCParams == null) {
             return;
         }
+        setFramework(5);
         mTRTCCloud.setListener(this);
         mTRTCCloud.enterRoom(mTRTCParams, TRTCCloudDef.TRTC_APP_SCENE_VIDEOCALL);
     }
