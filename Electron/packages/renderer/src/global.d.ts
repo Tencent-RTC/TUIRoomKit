69024a6071716a42e89374aecc5217b94bce3e56
sophiezzhou <sophiezzhou@tencent.com>
2022-07-05 19:54:56 +0800

    release: TUIRoom-Electron v1.0.2

--- Electron/packages/renderer/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-
-export { }
-
-declare global {
-  interface Window {
-    removeLoading: () => void
-    LibGenerateTestUserSig: (SDKAPPID: number, SECRETKEY: string, EXPIRETIME: number) => any;
-  }
-}
