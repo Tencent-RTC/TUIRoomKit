842cd0f440ec364124387a168d57024e6785cdb3
xinlxinli <xinlxinli@tencent.com>
2022-10-31 14:21:42 +0800

    【Web】update vue2 component

--- Web/vue2/src/TUIRoom/components/RoomFooter/ChatControl.vue
+++ Web/vue2/src/TUIRoom/components/RoomFooter/ChatControl.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="chat-control-container">
+    <el-badge
+      v-if="chatStore.unReadCount > 0"
+      :value="chatStore.unReadCount > 10 ? '10+' : chatStore.unReadCount"
+    >
+      <icon-button
+        title="聊天"
+        :icon-name="iconName"
+        @click-icon="toggleChatSidebar"
+      />
+    </el-badge>
     <icon-button
+      v-else
       title="聊天"
       :icon-name="iconName"
       @click-icon="toggleChatSidebar"
@@ -12,10 +23,12 @@
 import { computed } from 'vue';
 import IconButton from '../common/IconButton.vue';
 import { useBasicStore } from '../../stores/basic';
+import { useChatStore } from '../../stores/chat';
 import { storeToRefs } from 'pinia';
 import { ICON_NAME } from '../../constants/icon';
 
 const basicStore = useBasicStore();
+const chatStore = useChatStore();
 const { sidebarName } = storeToRefs(basicStore);
 
 const iconName = computed(() => (sidebarName.value === 'chat' ? ICON_NAME.ChatActive : ICON_NAME.Chat));
@@ -29,9 +42,14 @@ function toggleChatSidebar() {
 
   basicStore.setSidebarOpenStatus(true);
   basicStore.setSidebarName('chat');
+  chatStore.updateUnReadCount(0);
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+.chat-control-container .el-badge .el-badge__content {
+  top: 14px;
+  right: 24px;
+  background-color: #006EFF;
+}
 </style>
