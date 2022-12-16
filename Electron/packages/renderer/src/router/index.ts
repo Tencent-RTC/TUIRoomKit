c2646b2c17e5c41f9e624d3a339d2ffafa81de09
chardzhang <chardzhang@tencent.com>
2022-12-16 11:09:39 +0800

    【Electron】roomkit 构建安装包修复

--- Electron/packages/renderer/src/router/index.ts
+++ Electron/packages/renderer/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
+import Home from '@/views/home.vue';
+import Room from '@/views/room.vue';
+
 const routes = [
   {
     path: '/',
@@ -7,11 +10,11 @@ const routes = [
   },
   {
     path: '/home',
-    component: () => import('@/views/home.vue'),
+    component: Home,
   },
   {
     path: '/room',
-    component: () => import('@/views/room.vue'),
+    component: Room,
   },
 ];
 
