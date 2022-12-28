e1ca5857dddb37ee9981a093470dc1191b3c0178
xinlxinli <xinlxinli@tencent.com>
2022-12-28 10:55:52 +0800

    【Web】delete vue2

--- Web/vue2/vue.config.js
+++
@@ -1,5 +0,0 @@
-const { defineConfig } = require('@vue/cli-service');
-module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-  transpileDependencies: true,
-});
