c8abce451f02e1e965ee57ab29f1561d68f5fb7d
mileszzhang <mileszzhang@tencent.com>
2023-06-19 10:34:12 +0800

    chrore: deleteLogin

--- Web/vue3/src/views/login/index.ts
+++
@@ -1,7 +0,0 @@
-import isMobile from '../../TUIRoom/utils/useMediaValue';
-import loginH5 from './loginH5.vue';
-import loginPc from './loginPC.vue';
-
-const login = isMobile ? loginH5 : loginPc;
-
-export default login;
