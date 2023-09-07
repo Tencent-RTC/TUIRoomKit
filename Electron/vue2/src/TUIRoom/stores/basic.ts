c395b851e210b11d2dd71b22bd764af26070d917
jasperdai <jasperdai@tencent.com>
2023-09-07 18:26:20 +0800

    【Electron】update Electron TUIRoomKit

--- Electron/vue2/src/TUIRoom/stores/basic.ts
+++ Electron/vue2/src/TUIRoom/stores/basic.ts
@@ -192,6 +192,20 @@ export const useBasicStore = defineStore('basic', {
       this.sidebarName = '';
       this.masterUserId = '';
       this.localQuality = 0;
+      this.roomId = '';
+      this.sdkAppId = 0;
+      this.userId = '';
+      this.userSig = '';
+      this.userName = '';
+      this.avatarUrl = '';
+      this.useStringRoomId = false;
+      this.roomMode = 'FreeSpeech';
+      this.showApplyUserList = false;
+      this.isFrontCamera = true;
+      this.defaultTheme = 'black';
+      this.isSupportSwitchTheme = true;
+      this.showHeaderTool = true;
+      this.shareLink = '';
     },
   },
 });
