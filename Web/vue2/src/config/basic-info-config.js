2ccd8a2e45ca479719beb32a636674880b364feb
xinlxinli <xinlxinli@tencent.com>
2024-03-27 10:39:39 +0800

    【Web】update Web/vue2

--- Web/vue2/src/config/basic-info-config.js
+++ Web/vue2/src/config/basic-info-config.js
@@ -41,7 +41,7 @@ export const SDKAPPID = 0;
  * 注意：该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
  * 文档：https://cloud.tencent.com/document/product/647/17275#Server
  */
-export const SECRETKEY = '';
+export const SDKSECRETKEY = '';
 
 /**
  * Signature expiration time, which should not be too short
@@ -69,11 +69,11 @@ export const userInfo = {
 };
 
 export function getBasicInfo() {
-  if (SDKAPPID === Number(0) || SECRETKEY === String('')) {
+  if (SDKAPPID === Number(0) || SDKSECRETKEY === String('')) {
     alert('Please configure your SDKAPPID in config/basic-info-config.js');
     return;
   }
-  const generator = new LibGenerateTestUserSig(SDKAPPID, SECRETKEY, EXPIRETIME);
+  const generator = new LibGenerateTestUserSig(SDKAPPID, SDKSECRETKEY, EXPIRETIME);
   const userSig = generator.genTestUserSig(userInfo.userId);
   const { userId, userName, avatarUrl } = userInfo;
   return {
