51f5ffdef2b840ba228d5ed5d802c0a7d488e167
jasperdai <jasperdai@tencent.com>
2024-04-08 11:40:33 +0800

    【Electron】update Electron TUIRoomKit

--- Electron/vue2/src/TUIRoom/utils/environment.ts
+++ Electron/vue2/src/TUIRoom/utils/environment.ts
@@ -1,10 +1,23 @@
 
-export const isElectron = navigator.userAgent?.toLowerCase().indexOf(' electron/') > -1;
-declare let wx: any;
-export const isWeChat = (typeof wx !== 'undefined' && typeof wx.getSystemInfoSync === 'function' && Boolean(wx.getSystemInfoSync().fontSizeSetting));
-export const isMobile = window?.matchMedia('(max-width: 600px)').matches || isWeChat;
-export const isH5 = isMobile && !isWeChat;
+import { getPlatform } from '@tencentcloud/universal-api';
 
+
+declare const uni: any;
+
+export const isPC = getPlatform() === 'pc';
+
+export const isH5 = getPlatform() === 'h5';
+
+export const isWeChat = getPlatform() === 'wechat';
+
+export const isApp = getPlatform() === 'app';
+
+export const isUniFrameWork = typeof uni !== 'undefined';
+
+// H5、小程序、app 均认为是手机端产品，如果需要统一手机端 UI 样式，可以直接用 isMobile 控制
+export const isMobile = isH5 || isWeChat || isApp;
+
+export const isElectron = navigator.userAgent?.toLowerCase().indexOf(' electron/') > -1;
 
 // 微信浏览器
 export const isWeiXinBrowser = navigator && navigator.userAgent?.toLocaleLowerCase().indexOf('micromessenger') > -1;
