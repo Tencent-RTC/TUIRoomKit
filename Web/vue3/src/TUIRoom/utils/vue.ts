d54d05365d86cc71cf1b653f91724c352cba4737
xinlxinli <xinlxinli@tencent.com>
2024-04-22 19:46:47 +0800

    【Web】update Web/vue3

--- Web/vue3/src/TUIRoom/utils/vue.ts
+++
@@ -1,3 +0,0 @@
-import * as Vue from 'vue';
-
-export default Vue;
--- Web/vue3/src/main.ts
+++ Web/vue3/src/main.ts
@@ -2,10 +2,10 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import { createPinia } from 'pinia';
 import router from './router/index';
-import VueI18n from '@/TUIRoom/locales/index';
+import i18n from './locales';
 
 const app = createApp(App);
 app.use(router);
+app.use(i18n);
 app.use(createPinia());
-app.use(VueI18n);
 app.mount('#app');
