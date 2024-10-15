9e4660ce27802a8d9aa2096652b7a0202e2006d3
prenenerli <prenenerli@tencent.com>
2024-10-15 15:24:16 +0800

    【Qt】Remove Qt Project

--- Qt/RoomKit/Resource/Language/translatebat/lupdate.bat
+++
@@ -1,4 +0,0 @@
-%QTDIR%\bin\lupdate.exe ../../../RoomKit.pro -ts ../RoomKit_en.ts
-%QTDIR%\bin\lupdate.exe ../../../RoomKit.pro -ts ../RoomKit_zh.ts
-%QTDIR%\bin\linguist.exe ../RoomKit_zh.ts
-%QTDIR%\bin\linguist.exe ../RoomKit_en.ts
