75177db02c2a1d029fcc0420c4ce2fe53d4b19c5
yuanfazheng <yuanfazheng@tencent.com>
2024-12-27 17:30:25 +0800

    【MiniProgram】update Miniprogram

--- MiniProgram/wxmini_dev.sh
+++ MiniProgram/wxmini_dev.sh
@@ -4,6 +4,6 @@ npm init -y
 npm i @tencentcloud/trtc-component-wx
 cd roomkit
 npm init -y
-npm i @tencentcloud/tuiroom-engine-wx@2.8.1
+npm i @tencentcloud/tuiroom-engine-wx@2.8.2
 npm i @tencentcloud/chat
 npm i @tencentcloud/tui-core
