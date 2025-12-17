9f41af9bedd63b538de333b06d37aabb8d18a59d
yuanfazheng <yuanfazheng@tencent.com>
2025-12-17 14:37:55 +0800

    【Web】update Web Demo

--- Web/example/atomicx-vite-vue3-ts/src/components/RoomChat/CustomMessage.vue
+++ Web/example/atomicx-vite-vue3-ts/src/components/RoomChat/CustomMessage.vue
@@ -1,28 +1,24 @@
 <template>
   <div class="custom-message-container">
-    <div v-if="showNick" :class="['custom-message-nick', isMe && 'self']">
-      {{ userName }}
-    </div>
     <Message
       v-bind="$attrs"
+      :nick="userName"
       :message="message"
       :is-hidden-message-avatar="true"
-      :is-hidden-message-meta="!showMeta"
+      :is-hidden-message-meta="props.message.flow !== 'out'"
     />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, useAttrs } from 'vue';
+import { computed } from 'vue';
 import { Message } from 'tuikit-atomicx-vue3/chat';
 import { useRoomParticipantState } from 'tuikit-atomicx-vue3/room';
-import { MessageType } from 'tuikit-atomicx-vue3/types';
 import type { MessageModel } from 'tuikit-atomicx-vue3/types';
 
 interface Props {
   message: MessageModel;
 }
-const attrs = useAttrs();
 const props = defineProps<Props>();
 
 const { participantList } = useRoomParticipantState();
@@ -30,19 +26,6 @@ const userName = computed(() => {
   const participant = participantList.value.find(p => p.userId === props.message.from);
   return participant?.nameCard || participant?.userName || participant?.userId || props.message.nick || props.message.from;
 });
-
-const isMe = computed(() => props.message.flow === 'out');
-const isFirstInChunk = computed(() => attrs['is-first-in-chunk']);
-const isLastInChunk = computed(() => attrs['is-last-in-chunk']);
-const isSpecialMessage = computed(() => props.message.type === MessageType.TEXT
-  || props.message.type === MessageType.IMAGE
-  || props.message.type === MessageType.VIDEO
-  || props.message.type === MessageType.AUDIO
-  || props.message.type === MessageType.FILE);
-const isRevoked = computed(() => props.message.isRevoked);
-
-const showNick = computed(() => isSpecialMessage.value && isFirstInChunk.value && !isRevoked.value);
-const showMeta = computed(() => isLastInChunk.value && isMe.value);
 </script>
 
 <style lang="scss" scoped>
