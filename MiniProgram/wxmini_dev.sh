dae91e2c7b6d7596efedd87d718bcd466496fbc0
yuanfazheng <yuanfazheng@tencent.com>
2025-12-22 14:58:57 +0800

    【MiniProgram】update Miniprogram

--- MiniProgram/wxmini_dev.sh
+++ MiniProgram/wxmini_dev.sh
@@ -1,7 +1,7 @@
 cd dist/dev/mp-weixin
 cd unpackage/dist/dev/mp-weixin
 npm init -y
-npm i @tencentcloud/trtc-component-wx@^1.0.2 --no-package-lock
+npm i @tencentcloud/trtc-component-wx@^1.0.6 --no-package-lock
 cd roomkit
 npm init -y
 npm i @tencentcloud/tuiroom-engine-wx@~3.1.1 --no-package-lock
--- MiniProgram/wxmini_prod.bat
+++ MiniProgram/wxmini_prod.bat
@@ -8,4 +8,4 @@ if exist "dist\build\mp-weixin" (
   exit /b 1
 )
 
-npm init -y && npm i @tencentcloud/trtc-component-wx@^1.0.2 --no-package-lock && cd roomkit && npm init -y && npm i @tencentcloud/tuiroom-engine-wx@~3.1.1 --no-package-lock && npm i @tencentcloud/tui-core --no-package-lock
+npm init -y && npm i @tencentcloud/trtc-component-wx@^1.0.6 --no-package-lock && cd roomkit && npm init -y && npm i @tencentcloud/tuiroom-engine-wx@~3.1.1 --no-package-lock && npm i @tencentcloud/tui-core --no-package-lock
