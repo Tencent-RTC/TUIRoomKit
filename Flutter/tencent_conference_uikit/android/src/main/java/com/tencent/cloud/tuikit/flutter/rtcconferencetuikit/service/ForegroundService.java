4cfc350716ec7a84c4685effb26cf022ade2b465
Jeremiahandsome <995164912@qq.com>
2026-03-05 15:46:36 +0800

    【TUIRoomKit】【Flutter】Add missing imports

--- Flutter/tencent_conference_uikit/android/src/main/java/com/tencent/cloud/tuikit/flutter/rtcconferencetuikit/service/ForegroundService.java
+++ Flutter/tencent_conference_uikit/android/src/main/java/com/tencent/cloud/tuikit/flutter/rtcconferencetuikit/service/ForegroundService.java
@@ -1,17 +1,21 @@
 package com.tencent.cloud.tuikit.flutter.rtcconferencetuikit.service;
 
+import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ServiceInfo;
 import android.os.Build;
 import android.os.IBinder;
 import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 public class ForegroundService extends Service {
     private static final String TAG         = "KeepAliveService";
